f092ae36d538d499d42957f9839d265c3b4b2292
gokayari <gokayari@gmail.com>
2022-06-19 19:22:42 +0200

    CukesRunner.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         //tags = "@employee and not @admin"
-        tags = "@Regression"
+        tags = "@scenarioOutline"
 
 )
 public class CukesRunner {
