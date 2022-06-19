f4bd19c7ad33cbe00d53cc11433d987aea673c2d
gokayari <gokayari@gmail.com>
2022-06-19 19:02:08 +0200

    GoogleStepDefinitions

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -12,13 +12,31 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String searchKeyword) {
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+    }
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+        String expectedTitle = string + " - Google Suche";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+
+    }
+
+    //--
+
     @When("user types apple and clicks enter")
     public void userTypesAppleAndClicksEnter() {
         googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
 
     @Then("user sees apple in the google title")
-    public void userSeesAppleInTheGoogleTitle() {
+    public void user_sees_apple_in_the_google_title() {
         String expectedTitle = "apple - Google Suche";
         String actualTitle = Driver.getDriver().getTitle();
 
@@ -27,7 +45,7 @@ public class GoogleStepDefinitions {
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
-
+    //--
 
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
@@ -38,6 +56,7 @@ public class GoogleStepDefinitions {
         BrowserUtils.sleep(2);
 
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         //System.out.println("User is seeing title is Google");
