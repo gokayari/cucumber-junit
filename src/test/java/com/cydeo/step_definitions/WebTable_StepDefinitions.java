02cf7473e85b6882d0577d3230a392380abb6f76
gokayari <gokayari@gmail.com>
2022-06-20 20:23:26 +0200

    DataTable Step Definitions

--- src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -8,6 +8,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.Map;
+
 public class WebTable_StepDefinitions {
 
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
@@ -49,5 +51,20 @@ public class WebTable_StepDefinitions {
          */
 
         webTableLoginPage.login(u_name, pw);
+    }
+
+
+    @When("User enters below credentials")
+    public void user_enters_below_credentials(Map<String, String> credentials) {
+
+        System.out.println("credentials.get(\"username\") = " + credentials.get("username"));
+        System.out.println("credentials.get(\"password\") = " + credentials.get("password"));
+
+//        webTableLoginPage.inputUsername.sendKeys(credentials.get("username"));
+//        webTableLoginPage.inputPassword.sendKeys(credentials.get("password"));
+//        webTableLoginPage.loginButton.click();
+
+        //we can call our login utility method and pass values from map
+        webTableLoginPage.login( credentials.get("username"), credentials.get("password") );
     }
 }
